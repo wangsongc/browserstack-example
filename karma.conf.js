7230bc7481057b008817f2159863275c0dd4b196
wangsongc <40830929+wangsongc@users.noreply.github.com>
2020-10-15 20:27:03 +0800

    Update node.js.yml

--- karma.conf.js
+++ karma.conf.js
@@ -1,10 +1,67 @@
-// karma.conf.js
+// Karma configuration
+// Generated on Wed Oct 07 2015 12:43:21 GMT+0530 (IST)
+
 module.exports = function(config) {
   config.set({
-    // global config of your BrowserStack account
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+    // hostname for local
+    hostname: 'bs-local.com',
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['qunit'],
+
+    //plugins
+    plugins: ['karma-qunit', 'karma-browserstack-launcher'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'src/*.js',
+      'tests/*.js'
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress', 'BrowserStack'],
+
+
+    // web server host and port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
     browserStack: {
-      username: 'wangsongc1',
-      accessKey: 'yUceog3xetzq3ymiMKTC'
+      username: process.env.BROWSERSTACK_USERNAME,
+      accessKey: process.env.BROWSERSTACK_ACCESS_KEY,
+      apiClientEndpoint: 'https://api.browserstack.com'
     },
 
     // define browsers
@@ -12,18 +69,31 @@ module.exports = function(config) {
       bs_firefox_mac: {
         base: 'BrowserStack',
         browser: 'firefox',
-        browser_version: '21.0',
+        browser_version: '70.0',
         os: 'OS X',
-        os_version: 'Mountain Lion'
+        os_version: 'High Sierra'
       },
-      bs_iphone5: {
+      bs_iphoneX: {
         base: 'BrowserStack',
-        device: 'iPhone 5',
+        device: 'iPhone X',
         os: 'ios',
-        os_version: '6.0'
+        real_mobile: true,
+        os_version: '11.0'
       }
     },
 
-    browsers: ['bs_firefox_mac', 'bs_iphone5']
+    captureTimeout: 3e5,
+    browserDisconnectTolerance: 0,
+    browserDisconnectTimeout: 3e5,
+    browserSocketTimeout: 1.2e5,
+    browserNoActivityTimeout: 3e5,
+
+    browsers: ['bs_firefox_mac', 'bs_iphoneX'],
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true
   })
 }
---
+++ test/test.js
@@ -0,0 +1,9 @@
+// const isOdd = require('../src/odd.js')
+caps.setCapability("browserstack.local", "true")
+QUnit.test("is 1 odd", function( assert ) {
+  assert.ok(isOdd(1), 'One is an odd number');
+});
+
+QUnit.test("is 2 odd", function( assert ) {
+  assert.notOk(isOdd(2), 'two is an even number');
+});
